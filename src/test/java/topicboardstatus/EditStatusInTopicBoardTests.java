f97fbbd83a7f70c60761a744bfb9002e7d5584f7
Marat-Raz <mrazetdinov@briogroup.ru>
2025-03-04 18:58:34 +0300

    Add TopicBoardStatusBaseTests

--- src/test/java/topicboardstatus/EditStatusInTopicBoardTests.java
+++ src/test/java/topicboardstatus/EditStatusInTopicBoardTests.java
@@ -1,50 +1,18 @@
 package topicboardstatus;
 
-import static models.statuses.StatusesType.DEFAULT;
-import static models.topicboards.TopicBoardsType.DEFAULT_TOPIC_BOARDS;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import basetests.StartTests;
-import client.TopicBoardsClient;
-import client.TopicBoardStatusClient;
 import io.restassured.response.ValidatableResponse;
 import models.statuses.ResponseStatuses;
 import models.statuses.Statuses;
-import models.statuses.StatusesFactory;
-import models.topicboards.ResponseTopicBoards;
-import models.topicboards.TopicBoards;
-import models.topicboards.TopicBoardsFactory;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class EditStatusInTopicBoardTests extends StartTests {
+public class EditStatusInTopicBoardTests extends TopicBoardStatusBaseTests {
 
-  private static TopicBoardsClient topicBoardsClient = new TopicBoardsClient();
-  private static TopicBoardsFactory topicBoardsFactory = new TopicBoardsFactory();
-  private static TopicBoards topicBoard;
-  private static ValidatableResponse createTopicBoardsResponse;
-  private static String topicBoardId;
-  private static ValidatableResponse addStatusResponse;
   private static ValidatableResponse editStatusResponse;
-  private static TopicBoardStatusClient topicBoardStatusClient = new TopicBoardStatusClient();
-  private static StatusesFactory statusesFactory = new StatusesFactory();
-  private static Statuses status;
-  private static String statusId;
-
-  @BeforeAll
-  public static void createTopicBoardAndAddStatus() {
-    topicBoard = topicBoardsFactory.createTopicBoards(DEFAULT_TOPIC_BOARDS);
-    createTopicBoardsResponse = topicBoardsClient.createNewTopicBoard(projectId, topicBoard);
-    ResponseTopicBoards responseTopicBoards =
-        createTopicBoardsResponse.extract().as(ResponseTopicBoards.class);
-    topicBoardId = responseTopicBoards.getId();
-    status = statusesFactory.createStatuses(DEFAULT);
-    addStatusResponse = topicBoardStatusClient.addStatusToTopicBoard(topicBoardId, status);
-    statusId = addStatusResponse.extract().path("id");
-  }
 
   @Test
   @Tag(value = "smoke")
---
+++ src/test/java/topicboardstatus/TopicBoardStatusBaseTests.java
@@ -0,0 +1,44 @@
+package topicboardstatus;
+
+import static models.statuses.StatusesType.DEFAULT;
+import static models.topicboards.TopicBoardsType.DEFAULT_TOPIC_BOARDS;
+
+import basetests.StartTests;
+import client.TopicBoardStatusClient;
+import client.TopicBoardsClient;
+import io.restassured.response.ValidatableResponse;
+import models.statuses.ResponseStatuses;
+import models.statuses.Statuses;
+import models.statuses.StatusesFactory;
+import models.topicboards.ResponseTopicBoards;
+import models.topicboards.TopicBoards;
+import models.topicboards.TopicBoardsFactory;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TopicBoardStatusBaseTests extends StartTests {
+
+  protected static TopicBoardsClient topicBoardsClient = new TopicBoardsClient();
+  protected static TopicBoardsFactory topicBoardsFactory = new TopicBoardsFactory();
+  protected static TopicBoardStatusClient topicBoardStatusClient = new TopicBoardStatusClient();
+  protected static StatusesFactory statusesFactory = new StatusesFactory();
+  protected static TopicBoards topicBoard;
+  protected static ValidatableResponse createTopicBoardsResponse;
+  protected static String topicBoardId;
+  protected static ValidatableResponse addStatusResponse;
+  protected static Statuses status;
+  protected static String statusId;
+  protected static ResponseStatuses responseStatuses;
+
+  @BeforeAll
+  public static void createTopicBoardAndAddStatus() {
+    topicBoard = topicBoardsFactory.createTopicBoards(DEFAULT_TOPIC_BOARDS);
+    createTopicBoardsResponse = topicBoardsClient.createNewTopicBoard(projectId, topicBoard);
+    ResponseTopicBoards responseTopicBoards =
+        createTopicBoardsResponse.extract().as(ResponseTopicBoards.class);
+    topicBoardId = responseTopicBoards.getId();
+    status = statusesFactory.createStatuses(DEFAULT);
+    addStatusResponse = topicBoardStatusClient.addStatusToTopicBoard(topicBoardId, status);
+    responseStatuses = addStatusResponse.extract().as(ResponseStatuses.class);
+    statusId = responseStatuses.getId();
+  }
+}
