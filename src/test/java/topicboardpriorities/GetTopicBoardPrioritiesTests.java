5352bff3698b901156a24d54387e773302b08044
Marat-Raz <mrazetdinov@briogroup.ru>
2025-03-06 16:12:41 +0300

    Changed tests in the topicboardpriorities package

--- src/test/java/topicboardpriorities/GetTopicBoardPrioritiesTests.java
+++ src/test/java/topicboardpriorities/GetTopicBoardPrioritiesTests.java
@@ -1,21 +1,14 @@
 package topicboardpriorities;
 
-import static models.topicboards.TopicBoardsType.DEFAULT_TOPIC_BOARDS;
 import static models.priorities.PrioritiesType.DEFAULT;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import basetests.StartTests;
 import client.TopicBoardPrioritiesClient;
-import client.TopicBoardsClient;
 import io.restassured.response.ValidatableResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import models.topicboards.ResponseTopicBoards;
-import models.topicboards.TopicBoards;
-import models.topicboards.TopicBoardsFactory;
 import models.priorities.Priorities;
 import models.priorities.PrioritiesFactory;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,32 +16,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class GetTopicBoardPrioritiesTests extends StartTests {
+public class GetTopicBoardPrioritiesTests extends TopicBoardPrioritiesBaseTests {
 
-  private static TopicBoardsClient topicBoardsClient = new TopicBoardsClient();
-  private static TopicBoardsFactory topicBoardsFactory = new TopicBoardsFactory();
-  private static TopicBoards topicBoard;
-  private static ValidatableResponse createTopicBoardsResponse;
-  private static String topicBoardId;
-  private static ValidatableResponse addPrioritiesResponse;
   private static TopicBoardPrioritiesClient topicBoardPrioritiesClient = new TopicBoardPrioritiesClient();
-  private static final int prioritiesCount = 5;
   private ValidatableResponse getAllPriorities;
   private static List<Priorities> expectedPriorities = new ArrayList<>();
 
   @BeforeAll
-  public static void createTopicBoardAndAddPriority() {
-    topicBoard = topicBoardsFactory.createTopicBoards(DEFAULT_TOPIC_BOARDS);
-    createTopicBoardsResponse = topicBoardsClient.createNewTopicBoard(projectId, topicBoard);
-    ResponseTopicBoards responseTopicBoards =
-        createTopicBoardsResponse.extract().as(ResponseTopicBoards.class);
-    topicBoardId = responseTopicBoards.getId();
-
-    for (int i = 0; i < prioritiesCount; i++) {
+  public static void addPriorityToTopicBoard() {
+    expectedPriorities.add(priority);
+    for (int i = 0; i < 5; i++) {
       expectedPriorities.add(new PrioritiesFactory().createPriorities(DEFAULT));
     }
     for (Priorities status : expectedPriorities) {
-      addPrioritiesResponse = topicBoardPrioritiesClient.addPrioritiesToTopicBoard(topicBoardId, status);
+      addPrioritiesResponse = topicBoardPrioritiesClient.addPrioritiesToTopicBoard(topicBoardId,
+          status);
     }
   }
 
@@ -57,11 +39,9 @@ public class GetTopicBoardPrioritiesTests extends StartTests {
   @DisplayName("Получить все приоритеты доски задач")
   public void getTopicBoardPrioritiesTest() {
     getAllPriorities = topicBoardPrioritiesClient.getTopicBoardPriorities(topicBoardId);
-    List<Priorities> actualPriorities = Arrays.asList(getAllPriorities.extract().as(Priorities[].class));
+    List<Priorities> actualPriorities = Arrays.asList(
+        getAllPriorities.extract().as(Priorities[].class));
 
-    assertAll(
-        () -> assertEquals(prioritiesCount, actualPriorities.size()),
-        () -> assertEquals(new HashSet<>(expectedPriorities), new HashSet<>(actualPriorities))
-    );
+    assertEquals(new HashSet<>(expectedPriorities), new HashSet<>(actualPriorities));
   }
 }
---
+++ src/test/java/topicboardpriorities/TopicBoardPrioritiesBaseTests.java
@@ -0,0 +1,42 @@
+package topicboardpriorities;
+
+import static models.priorities.PrioritiesType.DEFAULT;
+import static models.topicboards.TopicBoardsType.DEFAULT_TOPIC_BOARDS;
+
+import basetests.StartTests;
+import client.TopicBoardPrioritiesClient;
+import client.TopicBoardsClient;
+import io.restassured.response.ValidatableResponse;
+import models.priorities.Priorities;
+import models.priorities.PrioritiesFactory;
+import models.topicboards.ResponseTopicBoards;
+import models.topicboards.TopicBoards;
+import models.topicboards.TopicBoardsFactory;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TopicBoardPrioritiesBaseTests extends StartTests {
+
+  protected static TopicBoardsClient topicBoardsClient = new TopicBoardsClient();
+  protected static TopicBoardsFactory topicBoardsFactory = new TopicBoardsFactory();
+  protected static TopicBoardPrioritiesClient topicBoardPrioritiesClient = new TopicBoardPrioritiesClient();
+  protected static PrioritiesFactory prioritiesFactory = new PrioritiesFactory();
+  protected static TopicBoards topicBoard;
+  protected static ValidatableResponse createTopicBoardsResponse;
+  protected static ValidatableResponse addPrioritiesResponse;
+  protected static Priorities priority;
+  protected static String topicBoardId;
+  protected static String priorityId;
+
+  @BeforeAll
+  public static void createTopicBoard() {
+    topicBoard = topicBoardsFactory.createTopicBoards(DEFAULT_TOPIC_BOARDS);
+    createTopicBoardsResponse = topicBoardsClient.createNewTopicBoard(projectId, topicBoard);
+    ResponseTopicBoards responseTopicBoards =
+        createTopicBoardsResponse.extract().as(ResponseTopicBoards.class);
+    topicBoardId = responseTopicBoards.getId();
+    priority = prioritiesFactory.createPriorities(DEFAULT);
+    addPrioritiesResponse = topicBoardPrioritiesClient.addPrioritiesToTopicBoard(topicBoardId,
+        priority);
+    priorityId = addPrioritiesResponse.extract().path("id");
+  }
+}
