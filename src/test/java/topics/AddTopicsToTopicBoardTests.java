97dfdc2a5e7a953c858349edfe05846d8e5f75ee
Marat-Raz <mrazetdinov@briogroup.ru>
2025-03-07 17:49:50 +0300

    fix

--- src/test/java/topics/AddTopicsToTopicBoardTests.java
+++ src/test/java/topics/AddTopicsToTopicBoardTests.java
@@ -1,5 +1,6 @@
 package topics;
 
+import static models.topics.TopicType.DEFAULT_TOPIC;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,12 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import io.restassured.response.ValidatableResponse;
 import models.topics.ResponseTopics;
 import models.topics.Topics;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class AddTopicsToTopicBoardTests extends TopicsBaseTests {
 
+  @BeforeEach
+  public void addTopic() {
+    topic = topicsFactory.createTopic(DEFAULT_TOPIC);
+    addTopicsResponse = topicsClient.createTopicInTopicBoard(topicBoardId, topic);
+    responseTopic = addTopicsResponse.extract().as(ResponseTopics.class);
+    defaultTopicId = responseTopic.getId();
+  }
 
   @Test
   @Tag(value = "smoke")
