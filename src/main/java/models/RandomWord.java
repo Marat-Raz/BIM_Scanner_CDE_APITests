25b2fc1dfc44e81bc65be028a38ec70ca4d6c31e
Marat-Raz <mrazetdinov@briogroup.ru>
2025-03-20 16:25:01 +0300

    generatePassword method added

--- src/main/java/models/RandomWord.java
+++ src/main/java/models/RandomWord.java
@@ -1,18 +1,25 @@
 package models;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class RandomWord {
 
-  private static final String LATIN_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  private static final String CYRILLIC_CHARS =
-      "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+  private static final String LATIN_LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+  private static final String LATIN_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  private static final String LATIN_CHARS = LATIN_LOWERCASE + LATIN_UPPERCASE;
+  private static final String CYRILLIC_LOWERCASE = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+  private static final String CYRILLIC_UPPERCASE = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+  private static final String CYRILLIC_CHARS = CYRILLIC_LOWERCASE + CYRILLIC_UPPERCASE;
   private static final String UZBEK_CHARS = "ўқғҳЎҚҒҲ";
-  private static final String NUMBERS = "0123456789";
+  private static final String DIGITS = "0123456789";
   private static final String SPECIAL_CHARS = "!@#$%^&*()_+-={}[]|;:',.<>/?`~«»";
-
   private static final String ALL_CHARS =
-      LATIN_CHARS + CYRILLIC_CHARS + UZBEK_CHARS + SPECIAL_CHARS + NUMBERS;
+      LATIN_CHARS + CYRILLIC_CHARS + UZBEK_CHARS + SPECIAL_CHARS + DIGITS;
+  private static final SecureRandom random = new SecureRandom();
 
   public static String generateWord(int minLength, int maxLength, String chars) {
     if (minLength > maxLength) {
@@ -47,6 +54,45 @@ public class RandomWord {
 
   public static String randomAllCharacters(int minLength, int maxLength) {
     return generateWord(minLength, maxLength, ALL_CHARS);
+  }
+
+  public static String randomLatinAndNumberCharacters(int minLength, int maxLength) {
+    return generateWord(minLength, maxLength, LATIN_CHARS + DIGITS);
+  }
+
+  public static String generatePassword(int minLength, int maxLength) {
+    if (minLength < 6) {
+      throw new IllegalArgumentException("Минимальная длина пароля должна быть не менее 6 символов");
+    }
+    if (maxLength < minLength) {
+      throw new IllegalArgumentException("Максимальная длина должна быть больше или "
+          + "равна минимальной длине.");
+    }
+
+    int length = minLength + random.nextInt(maxLength - minLength + 1);
+    List<Character> passwordChars = new ArrayList<>();
+    passwordChars.add(getRandomChar(LATIN_LOWERCASE));
+    passwordChars.add(getRandomChar(LATIN_UPPERCASE));
+    passwordChars.add(getRandomChar(DIGITS));
+    passwordChars.add(getRandomChar(SPECIAL_CHARS));
+
+    for (int i = 4; i < length; i++) {
+      passwordChars.add(getRandomChar(ALL_CHARS));
+    }
+
+    Collections.shuffle(passwordChars, random);
+
+    StringBuilder password = new StringBuilder();
+    for (char c : passwordChars) {
+      password.append(c);
+    }
+
+    return password.toString();
+  }
+
+  private static char getRandomChar(String charSet) {
+    int randomIndex = random.nextInt(charSet.length());
+    return charSet.charAt(randomIndex);
   }
 
 }
--- src/main/java/models/user/UserFactory.java
+++ src/main/java/models/user/UserFactory.java
@@ -7,15 +7,16 @@ import models.RandomWord;
 public class UserFactory {
 
   private final String NULL = null; // На данном этапе тестирования принимаем так
-  private String userName = RandomWord.randomAllCharacters(6, 248);
+  private String userName = RandomWord.randomLatinAndNumberCharacters(6, 248);
   private String name = NULL;
   private String surname = NULL;
-  private String emailAddress = RandomWord.randomAllCharacters(3, 247) + "@mail.com";
+  private String emailAddress = RandomWord.randomLatinAndNumberCharacters(3, 247) + "@mail.com";
   private String phoneNumber = NULL;
   private boolean active = true; // пока делаем так, далее при необходимости будем менять
   private boolean lockoutEnabled = true;  // пока делаем так, далее при необходимости будем менять
   private String[] roleNames = null;   // пока для простоты делаем так, далее при необходимости будем менять
-  private String password = RandomWord.randomAllCharacters(8, 128);
+  private String password = RandomWord.generatePassword(8, 128);
+  // todo нужно обязательность символов указывать: цифры, прописные, строчные, спецсимволы
 
   public User createUser(UserType userType) {
     switch (userType) {
@@ -29,9 +30,9 @@ public class UserFactory {
         return new User(null, name, surname, emailAddress, phoneNumber, active,
             lockoutEnabled, null, null);
       case NEW_USER:
-        return new User("newUser_" + RandomWord.randomAllCharacters(0, 247),
-            RandomWord.randomAllCharacters(0, 64), RandomWord.randomAllCharacters(0, 64),
-            "newEmail" + RandomWord.randomAllCharacters(3, 239) + "@mail.com",
+        return new User("newUser_" + RandomWord.randomLatinCharacters(0, 247),
+            RandomWord.randomLatinCharacters(0, 64), RandomWord.randomAllCharacters(0, 64),
+            "newEmail" + RandomWord.randomLatinAndNumberCharacters(3, 239) + "@mail.com",
             phoneNumber, active, lockoutEnabled, roleNames,
             "newPassword" + RandomWord.randomAllCharacters(8, 117));
       case USER_WITH_REAL_E_MAIL:
