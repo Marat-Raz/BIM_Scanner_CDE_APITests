53f94a017830236b42abace3d68f0b75fdf47890
Marat-Raz <mrazetdinov@briogroup.ru>
2025-03-24 14:31:12 +0300

    change coverImage tests

--- src/test/java/basetests/RestAssuredFilterSwitcher.java
+++ src/test/java/basetests/RestAssuredFilterSwitcher.java
@@ -10,23 +10,19 @@ import java.util.List;
 
 public class FilterSwitcher {
 
-  // Метод для временной замены фильтров
   public static void withTemporaryFilters(Runnable action) {
-    // Сохраняем текущие фильтры
-    List<Filter> originalFilters = RestAssured.filters();
 
+    List<Filter> originalFilters = RestAssured.filters();
     try {
-      // Устанавливаем новые фильтры
       RestAssured.filters(
           new RequestLoggingFilter(LogDetail.URI),
           new ResponseLoggingFilter(LogDetail.STATUS),
           new AllureRestAssured()
       );
 
-      // Выполняем действие с новыми фильтрами
       action.run();
+
     } finally {
-      // Возвращаем оригинальные фильтры
       RestAssured.replaceFiltersWith(originalFilters);
     }
   }
